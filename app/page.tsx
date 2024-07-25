86a1a24fe6d94aa44b21abd05787541214c847b5
Paul Bond <git@paulsbond.co.uk>
2024-07-26 00:11:32 +0200

    Projects page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import { Box, Container, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 
 export default function ProjectsPage() {
   return (
-    <>
-      <h1>Projects</h1>
-      <Button variant="contained">New</Button>
-      <Button variant="outlined">Import</Button>
-    </>
+    <Container>
+      <Box>
+        <Typography variant="h4" component="h1">
+          Projects
+        </Typography>
+        <Button variant="contained">New</Button>
+        <Button variant="outlined">Import</Button>
+      </Box>
+    </Container>
   );
 }
