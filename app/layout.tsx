7e07d4d0ee9aecfc5fba0671a67c0c0ad191008e
Paul Bond <git@paulsbond.co.uk>
2024-07-25 18:18:42 +0200

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'CCP4',
+  description: 'Software for Macromolecular X-Ray Crystallography',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>CCP4</h1>
+      <p>Software for Macromolecular X-Ray Crystallography</p>
+    </div>
+  );
+}
