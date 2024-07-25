1d99d092ddeb99840d3336e29457d97b26b5f4d6
Paul Bond <git@paulsbond.co.uk>
2024-07-25 23:00:54 +0200

    Added MUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
-import { PropsWithChildren } from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export const metadata = {
   title: "CCP4",
