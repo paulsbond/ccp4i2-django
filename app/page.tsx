79de68dff62f719f4e63c7e0d546dd80586dd0a4
Paul Bond <git@paulsbond.co.uk>
2024-07-26 10:08:49 +0200

    Updated projects page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
-import { Box, Container, Typography } from "@mui/material";
+import { AppBar, Container, Stack, Toolbar, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
+import { Add, Upload } from "@mui/icons-material";
 
 export default function ProjectsPage() {
   return (
-    <Container>
-      <Box>
-        <Typography variant="h4" component="h1">
-          Projects
-        </Typography>
-        <Button variant="contained">New</Button>
-        <Button variant="outlined">Import</Button>
-      </Box>
-    </Container>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="span">
+            CCP4
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ my: 3 }}>
+        <Stack spacing={2}>
+          <Typography variant="h4" component="h1">
+            Projects
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" startIcon={<Add />}>
+              New
+            </Button>
+            <Button variant="outlined" startIcon={<Upload />} disabled>
+              Import
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
   );
 }
