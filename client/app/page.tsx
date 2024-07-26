7a3e9766be39f21d3dd72a9a7676b16dfdabe711
Paul Bond <git@paulsbond.co.uk>
2024-07-26 17:32:07 +0200

    Fetching a list of projects

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,28 @@
 "use client";
 import { Button, Stack } from "@mui/material";
 import { Add, Upload } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+
+class Project {
+  constructor(
+    public id: number,
+    public uuid: string,
+    public name: string,
+    public created: Date
+  ) {}
+}
 
 export default function ProjectsPage() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/projects")
+      .then((response) => response.json())
+      .then((data: Project[]) => {
+        setProjects(data);
+      });
+  }, []);
+
   function addProject() {
     console.log("Add project");
   }
@@ -16,6 +36,11 @@ export default function ProjectsPage() {
         <Button variant="outlined" startIcon={<Upload />} disabled>
           Import
         </Button>
+      </Stack>
+      <Stack>
+        {projects.map((project) => (
+          <div key={project.id}>{project.name}</div>
+        ))}
       </Stack>
     </Stack>
   );
