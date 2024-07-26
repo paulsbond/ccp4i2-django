bb2b172bbee4decc10b98a2793e7d5e0cdfc655e
Paul Bond <git@paulsbond.co.uk>
2024-07-26 13:55:57 +0200

    Add project function

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import { Button, Stack, Typography } from "@mui/material";
+"use client";
+import { Button, Stack } from "@mui/material";
 import { Add, Upload } from "@mui/icons-material";
 
 export default function ProjectsPage() {
+  function addProject() {
+    console.log("Add project");
+  }
+
   return (
     <Stack spacing={2}>
       <Stack direction="row" spacing={2}>
-        <Button variant="contained" startIcon={<Add />}>
+        <Button variant="contained" startIcon={<Add />} onClick={addProject}>
           New
         </Button>
         <Button variant="outlined" startIcon={<Upload />} disabled>
