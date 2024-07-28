536aaf6167b7f37f9899678b2c2eddee35c2e678
Paul Bond <git@paulsbond.co.uk>
2024-07-28 13:46:45 +0100

    Post function

--- client/app/api.ts
+++ client/app/api.ts
@@ -1,10 +1,5 @@
 import useSWR from "swr";
 
-async function fetcher(input: string | URL | globalThis.Request, init?: RequestInit) {
-    const response = await fetch(input, init);
-    return response.json();
-};
-
 function fullUrl(url: string | URL): string {
     url = new URL(url, "http://127.0.0.1:8000");
     if (url.pathname.charAt(url.pathname.length - 1) !== '/')
@@ -12,7 +7,27 @@ function fullUrl(url: string | URL): string {
     return url.href;
 };
 
+async function fetcher(
+    input: string | URL | globalThis.Request,
+    init?: RequestInit
+): Promise<any> {
+    const response = await fetch(input, init);
+    return response.json();
+};
+
 export function get<T>(url: string): T | undefined {
     const response = useSWR<T>(fullUrl(url), fetcher);
     return response.data;
+};
+
+export async function post<T>(url: string, body: any = {}): Promise<T> {
+    const response = await fetch(fullUrl(url), {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    });
+    return response.json() as T;
 };
