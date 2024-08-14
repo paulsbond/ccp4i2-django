450cb8ad7536bb2115db5d089d09873d65ad025a
Paul Bond <git@paulsbond.co.uk>
2024-08-14 22:21:44 +0100

    Updated import paths

--- server/ccp4ui/db/models.py
+++ server/ccp4ui/db/models.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+from django.conf import settings
 from django.db.models import (
     CASCADE,
     BigIntegerField,
@@ -10,6 +11,7 @@ from django.db.models import (
     UUIDField,
 )
 from django.utils import timezone
+from ..utils import puid
 
 
 class Project(Model):
@@ -21,9 +23,16 @@ class Project(Model):
         return f"{self.id} {self.name}"
 
 
+def import_path(instance, filename):
+    path = f"{instance.project.id}/imports/{puid()}/{filename}"
+    while (settings.MEDIA_ROOT / path).exists():
+        path = f"{instance.project.id}/imports/{puid()}/{filename}"
+    return path
+
+
 class File(Model):
     project = ForeignKey(Project, on_delete=CASCADE, related_name="files")
-    file = FileField(upload_to="imports")
+    file = FileField(upload_to=import_path)
     name = TextField()
     size = BigIntegerField()
 
