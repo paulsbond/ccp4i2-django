261d5d6b729e678fa6b5c1b5ce3a50772a152ed2
Paul Bond <git@paulsbond.co.uk>
2024-08-15 14:35:05 +0100

    Disable interactive tooltips

--- client/app/theme.ts
+++ client/app/theme.ts
@@ -1,17 +1,24 @@
-'use client';
-import { Roboto } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
+"use client";
+import { Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
 
 const roboto = Roboto({
-    weight: ['300', '400', '500', '700'],
-    subsets: ['latin'],
-    display: 'swap',
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const theme = createTheme({
-    typography: {
-        fontFamily: roboto.style.fontFamily,
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiTooltip: {
+      defaultProps: {
+        disableInteractive: true,
+      },
     },
+  },
 });
 
 export default theme;
