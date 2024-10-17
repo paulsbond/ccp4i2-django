336cfbec33f32a1af96c373f043b6f38d68426d5
Paul Bond <git@paulsbond.co.uk>
2024-10-17 14:47:58 +0100

    Requests session cache

--- server/ccp4ui/lib/utils.py
+++ server/ccp4ui/lib/utils.py
@@ -4,15 +4,17 @@ from pathlib import Path
 from random import choice
 from string import ascii_letters, digits
 from tempfile import NamedTemporaryFile
-import requests
-import requests_cache
+from requests_cache import CachedSession
+from ..config.settings import USER_DIR
+
+
+_CHARS = ascii_letters + digits
+_HTTP = CachedSession(USER_DIR / "http-cache")
 
 
 def download(url: str, path: str | PathLike):
-    "Download a file to a path in 1 MB chunks. Downloads are cached for 24 hours."
-    requests_cache.install_cache(expire_after=86_400)
-    requests_cache.delete(expired=True)
-    response = requests.get(url, allow_redirects=True, stream=True, timeout=30)
+    "Download a file to a path in 1 MB chunks. Downloads are cached."
+    response = _HTTP.get(url, allow_redirects=True, stream=True, timeout=30)
     response.raise_for_status()
     with open(path, "wb") as writer:
         for chunk in response.iter_content(chunk_size=1_000_000):
@@ -21,8 +23,7 @@ def download(url: str, path: str | PathLike):
 
 def puid(length: int = 10):
     "Probably Unique Identifier"
-    chars = ascii_letters + digits
-    return "".join(choice(chars) for _ in range(length))
+    return "".join(choice(_CHARS) for _ in range(length))
 
 
 @contextmanager
