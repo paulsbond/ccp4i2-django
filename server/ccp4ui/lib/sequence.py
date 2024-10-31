3285d8392e022469737157fa0e71cd47c6b349f3
Paul Bond <git@paulsbond.co.uk>
2024-10-31 14:28:07 -0500

    Sequence parsing

--- server/ccp4ui/lib/sequence.py
+++ server/ccp4ui/lib/sequence.py
@@ -1,80 +1,109 @@
 from functools import cache
 
 
-L_PEPTIDE_NAMES = {
-    "A": "ALA",
-    "B": "ASX",
-    "C": "CYS",
-    "D": "ASP",
-    "E": "GLU",
-    "F": "PHE",
-    "G": "GLY",
-    "H": "HIS",
-    "I": "ILE",
-    "K": "LYS",
-    "L": "LEU",
-    "M": "MET",
-    "N": "ASN",
-    "O": "PYL",
-    "P": "PRO",
-    "Q": "GLN",
-    "R": "ARG",
-    "S": "SER",
-    "T": "THR",
-    "U": "SEC",
-    "V": "VAL",
-    "W": "TRP",
-    "X": "UNK",
-    "Y": "TYR",
-    "Z": "GLX",
-}
+class SequenceType:
+    def __init__(self, name: str, codes: dict[str, str]):
+        self.name = name
+        self.codes = codes
 
-D_PEPTIDE_NAMES = {
-    "A": "DAL",
-    "C": "DCY",
-    "D": "DAS",
-    "E": "DGL",
-    "F": "DPN",
-    "G": "GLY",
-    "H": "DHI",
-    "I": "DIL",
-    "K": "DLY",
-    "L": "DLE",
-    "M": "MED",
-    "N": "DSG",
-    "P": "DPR",
-    "Q": "DGN",
-    "R": "DAR",
-    "S": "DSN",
-    "T": "DTH",
-    "V": "DVA",
-    "W": "DTR",
-    "Y": "DTY",
-}
+    def parse(self, sequence: str) -> list[str]:
+        "Parse a string of one-letter codes to a list of residue names"
+        names = []
+        for code in sequence.strip().strip("-*"):
+            if code in {" ", "-"}:
+                continue
+            if code not in self.codes:
+                raise ValueError(f"'{code}' is not a valid {self.name} code")
+            names.append(self.codes[code])
+        return names
 
-DNA_NAMES = {
-    "A": "DA",
-    "C": "DC",
-    "G": "DG",
-    "I": "DI",
-    "T": "DT",
-    "U": "DU",
-    "X": "DN",
-}
 
-RNA_NAMES = {
-    "A": "A",
-    "C": "C",
-    "G": "G",
-    "I": "I",
-    "U": "U",
-    "X": "N",
-}
+L_PEPTIDE = SequenceType(
+    "L-Peptide",
+    {
+        "A": "ALA",
+        "B": "ASX",
+        "C": "CYS",
+        "D": "ASP",
+        "E": "GLU",
+        "F": "PHE",
+        "G": "GLY",
+        "H": "HIS",
+        "I": "ILE",
+        "K": "LYS",
+        "L": "LEU",
+        "M": "MET",
+        "N": "ASN",
+        "O": "PYL",
+        "P": "PRO",
+        "Q": "GLN",
+        "R": "ARG",
+        "S": "SER",
+        "T": "THR",
+        "U": "SEC",
+        "V": "VAL",
+        "W": "TRP",
+        "X": "UNK",
+        "Y": "TYR",
+        "Z": "GLX",
+    },
+)
+
+D_PEPTIDE = SequenceType(
+    "D-Peptide",
+    {
+        "A": "DAL",
+        "C": "DCY",
+        "D": "DAS",
+        "E": "DGL",
+        "F": "DPN",
+        "G": "GLY",
+        "H": "DHI",
+        "I": "DIL",
+        "K": "DLY",
+        "L": "DLE",
+        "M": "MED",
+        "N": "DSG",
+        "P": "DPR",
+        "Q": "DGN",
+        "R": "DAR",
+        "S": "DSN",
+        "T": "DTH",
+        "V": "DVA",
+        "W": "DTR",
+        "Y": "DTY",
+    },
+)
+
+DNA = SequenceType(
+    "DNA",
+    {
+        "A": "DA",
+        "C": "DC",
+        "G": "DG",
+        "I": "DI",
+        "T": "DT",
+        "U": "DU",
+        "X": "DN",
+    },
+)
+
+RNA = SequenceType(
+    "RNA",
+    {
+        "A": "A",
+        "C": "C",
+        "G": "G",
+        "I": "I",
+        "U": "U",
+        "X": "N",
+    },
+)
 
 ONE_LETTER_CODES = {
     name: code
-    for code_names in (L_PEPTIDE_NAMES, D_PEPTIDE_NAMES, DNA_NAMES, RNA_NAMES)
-    for code, name in code_names.items()
+    for sequence_type in (L_PEPTIDE, D_PEPTIDE, DNA, RNA)
+    for code, name in sequence_type.codes.items()
 }
 
 
--- server/ccp4ui/tests/lib/test_sequence.py
+++ server/ccp4ui/tests/lib/test_sequence.py
@@ -1,8 +1,8 @@
-from pytest import mark
-from ...lib.sequence import code1
+import pytest
+from ...lib.sequence import code1, SequenceType, L_PEPTIDE, D_PEPTIDE, DNA, RNA
 
 
-@mark.parametrize(
+@pytest.mark.parametrize(
     ("resname", "expected"),
     [
         ("", None),
@@ -26,3 +26,38 @@ from ...lib.sequence import code1
 )
 def test_code1(resname, expected):
     assert code1(resname) == expected
+
+
+@pytest.mark.parametrize(
+    ("sequence_type", "sequence", "expected"),
+    [
+        (L_PEPTIDE, "AA", ["ALA", "ALA"]),
+        (L_PEPTIDE, "AA*", ["ALA", "ALA"]),
+        (L_PEPTIDE, "\nAA\n", ["ALA", "ALA"]),
+        (L_PEPTIDE, " A A ", ["ALA", "ALA"]),
+        (L_PEPTIDE, "-A-A-", ["ALA", "ALA"]),
+        (L_PEPTIDE, "BOUXZ", ["ASX", "PYL", "SEC", "UNK", "GLX"]),
+        (D_PEPTIDE, "ACDEF", ["DAL", "DCY", "DAS", "DGL", "DPN"]),
+        (DNA, "ACGITUX", ["DA", "DC", "DG", "DI", "DT", "DU", "DN"]),
+        (RNA, "ACGIUX", ["A", "C", "G", "I", "U", "N"]),
+    ],
+)
+def test_parse(sequence_type: SequenceType, sequence: str, expected: list[str]):
+    assert sequence_type.parse(sequence) == expected
+
+
+@pytest.mark.parametrize(
+    ("sequence_type", "sequence"),
+    [
+        (L_PEPTIDE, "J"),
+        (L_PEPTIDE, "A/A"),
+        (L_PEPTIDE, "A,A"),
+        (L_PEPTIDE, "A(ALA)A"),
+        (D_PEPTIDE, "X"),
+        (DNA, "J"),
+        (RNA, "T"),
+    ],
+)
+def test_parse_error(sequence_type: SequenceType, sequence: str):
+    with pytest.raises(ValueError):
+        sequence_type.parse(sequence)
