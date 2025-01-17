230a02530a9410fe5bb2291576de803a13577a2f
Martin Noble <martinemnoble1@mac.com>
2025-01-17 11:07:05 +0000

    Removing unused imports

--- server/ccp4x/lib/job_utils/unset_output_data.py
+++ server/ccp4x/lib/job_utils/unset_output_data.py
@@ -1,9 +1,6 @@
 import logging
 from ccp4i2.core import CCP4PluginScript
 from core import CCP4File
-from ...db.ccp4i2_django_wrapper import using_django_pm
-from .save_params_for_job import save_params_for_job
-from .remove_container_default_values import remove_container_default_values
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(f"ccp4x:{__name__}")
