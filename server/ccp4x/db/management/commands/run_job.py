e3edf191de0e31669ece069239d45d6b29436d7e
Martin Noble <martinemnoble1@mac.com>
2025-01-17 12:13:44 +0000

    copilot assisted code path simplification

--- server/ccp4x/db/management/commands/run_job.py
+++ server/ccp4x/db/management/commands/run_job.py
@@ -15,48 +15,43 @@ class Command(BaseCommand):
         parser.add_argument("-ju", "--jobuuid", help="Job UUID", type=str)
         parser.add_argument("-pn", "--projectname", help="Project name", type=str)
         parser.add_argument("-pi", "--projectid", help="Integer project id", type=int)
-        parser.add_argument("-pu", "--projectuuid", help="Project uuid", type=int)
+        parser.add_argument("-pu", "--projectuuid", help="Project uuid", type=str)
         parser.add_argument("-jn", "--jobnumber", help="Job number", type=str)
         parser.add_argument("-d", "--detach", help="Detach job", action="store_true")
 
     def handle(self, *args, **options):
-        the_job = None
-        if options["jobid"] is not None:
-            the_job = Job.objects.get(id=options["jobid"])
-        elif options["jobuuid"] is not None:
-            the_job = Job.objects.get(uuid=uuid.UUID(options["jobuuid"]))
-        elif options["projectname"] is not None:
-            if options["jobnumber"] is not None:
-                the_project = Project.objects.get(name=options["projectname"])
-                the_job = Job.objects.get(
-                    number=options["jobnumber"], project=the_project
-                )
-        elif options["projectid"] is not None:
-            if options["jobnumber"] is not None:
-                the_project = Project.objects.get(id=options["projectid"])
-                the_job = Job.objects.get(
-                    number=options["jobnumber"], project=the_project
-                )
-        elif options["projectuuid"] is not None:
-            if options["jobnumber"] is not None:
-                the_project = Project.objects.get(
-                    uuid=uuid.UUID(options["projectuuid"])
-                )
-                the_job = Job.objects.get(
-                    number=options["jobnumber"], project=the_project
-                )
+        try:
+            the_job = self.get_job(options)
+        except (Job.DoesNotExist, Project.DoesNotExist) as e:
+            self.stderr.write(self.style.ERROR(str(e)))
+            return
 
-        if the_job:
-            if options["detach"]:
-                _ = subprocess.Popen(
-                    [
-                        "ccp4-python",
-                        "manage.py",
-                        "run_job",
-                        "-ju",
-                        f"{str(the_job.uuid)}",
-                    ],
-                    start_new_session=True,
-                )
-            else:
-                run_job(the_job.uuid)
+        if options["detach"]:
+            subprocess.Popen(
+                [
+                    "ccp4-python",
+                    "manage.py",
+                    "run_job",
+                    "-ju",
+                    f"{str(the_job.uuid)}",
+                ],
+                start_new_session=True,
+            )
+        else:
+            run_job(the_job.uuid)
+
+    def get_job(self, options):
+        if options["jobid"] is not None:
+            return Job.objects.get(id=options["jobid"])
+        if options["jobuuid"] is not None:
+            return Job.objects.get(uuid=uuid.UUID(options["jobuuid"]))
+        if options["projectname"] is not None and options["jobnumber"] is not None:
+            the_project = Project.objects.get(name=options["projectname"])
+            return Job.objects.get(number=options["jobnumber"], project=the_project)
+        if options["projectid"] is not None and options["jobnumber"] is not None:
+            the_project = Project.objects.get(id=options["projectid"])
+            return Job.objects.get(number=options["jobnumber"], project=the_project)
+        if options["projectuuid"] is not None and options["jobnumber"] is not None:
+            the_project = Project.objects.get(uuid=uuid.UUID(options["projectuuid"]))
+            return Job.objects.get(number=options["jobnumber"], project=the_project)
+        raise Job.DoesNotExist("No job found with the provided criteria.")
