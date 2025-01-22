286c6cf2599fa7410f493b3ef78736946218edcd
Martin Noble <martinemnoble1@mac.com>
2025-01-22 17:39:04 +0000

    Continuiss towards tasks

--- client/app/components/task/task-elements/number.tsx
+++ client/app/components/task/task-elements/number.tsx
@@ -1,7 +1,16 @@
+import { useMemo } from "react";
 import { CCP4i2TaskElementProps } from "./task-element";
 
 export const CCP4i2NumberTaskElement: React.FC<CCP4i2TaskElementProps> = (
   props
 ) => {
-  return <span>Number item</span>;
+  const { paramsXML, itemName } = props;
+  const value = useMemo(() => {
+    if (paramsXML && itemName) {
+      const valueNode = $(paramsXML).find(itemName);
+      return valueNode.text();
+    }
+    return "Unknown";
+  }, [props]);
+  return <span>{`Number ${itemName} ${value}`}</span>;
 };
--- client/app/components/task/task-elements/task-element.tsx
+++ client/app/components/task/task-elements/task-element.tsx
@@ -2,6 +2,7 @@ import { Job } from "../../../models";
 import { xml2js } from "xml-js";
 import { CCP4i2NumberTaskElement } from "./number";
 import { useMemo } from "react";
+import { Typography } from "@mui/material";
 
 export interface CCP4i2TaskElementProps {
   job: Job;
@@ -11,11 +12,30 @@ export interface CCP4i2TaskElementProps {
   itemName?: string;
 }
 export const CCP4i2TaskElement: React.FC<CCP4i2TaskElementProps> = (props) => {
-  const elementType = useMemo(() => {
+  const elementType = useMemo<string | null | undefined>(() => {
     if (props.defXML && props.itemName) {
-      const entry = props.defXML.find(`[id=${props.itemName}]`);
-      console.log({ ...props, entry });
+      const $itemNode = props.defXML.find(`[id=${props.itemName}]`);
+      if ($itemNode.toArray().length == 1) {
+        const $classNode = $($itemNode).find("className");
+        return $classNode.get(0)?.textContent;
+      } else return null;
     }
+    return null;
   }, [props]);
-  return <CCP4i2NumberTaskElement {...props} />;
+
+  const interfaceElement = useMemo(() => {
+    switch (elementType) {
+      case "CInt":
+        return <CCP4i2NumberTaskElement {...props} />;
+      default:
+        return <span>Unknown elementType</span>;
+    }
+  }, [elementType]);
+
+  return (
+    <>
+      <Typography>{elementType}</Typography>
+      {interfaceElement}
+    </>
+  );
 };
