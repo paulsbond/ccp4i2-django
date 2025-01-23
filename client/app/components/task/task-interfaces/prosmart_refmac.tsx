6474bdd3f6a905d0067a78da3e2188049b120186
Martin Noble <martinemnoble1@mac.com>
2025-01-23 15:15:53 +0000

    Styling components in prosmart_refmac interface

--- client/app/components/task/task-interfaces/prosmart_refmac.tsx
+++ client/app/components/task/task-interfaces/prosmart_refmac.tsx
@@ -5,8 +5,16 @@ import { CCP4i2TaskElement } from "../task-elements/task-element";
 const TaskInterface: React.FC<CCP4i2TaskInterfaceProps> = (props) => {
   return (
     <Paper>
-      <CCP4i2TaskElement itemName="NCYCLES" {...props} />
-      <CCP4i2TaskElement itemName="REFINEMENT_MODE" {...props} />
+      <CCP4i2TaskElement
+        itemName="NCYCLES"
+        {...props}
+        sx={{ m: 2, maxWidth: "15rem" }}
+      />
+      <CCP4i2TaskElement
+        itemName="REFINEMENT_MODE"
+        {...props}
+        sx={{ m: 2, maxWidth: "15rem" }}
+      />
     </Paper>
   );
 };
