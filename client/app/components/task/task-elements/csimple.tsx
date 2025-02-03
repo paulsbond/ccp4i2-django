048686af64e65e6c6fc0c16561986db7a13977a7
Martin Noble <martinemnoble1@mac.com>
2025-02-03 13:18:32 +0000

    Making csimple look at itemName rather than item

--- client/app/components/task/task-elements/csimple.tsx
+++ client/app/components/task/task-elements/csimple.tsx
@@ -3,13 +3,21 @@ import { CCP4i2TaskElementProps } from "./task-element";
 import { CSimpleTextFieldElement } from "./csimple-textfield";
 import { CSimpleAutocompleteElement } from "./csimple-autocomplete";
 import { Typography } from "@mui/material";
+import { useApi } from "../../../api";
+import { useTaskItem } from "../task-utils";
 
 export interface CCP4i2CSimpleElementProps extends CCP4i2TaskElementProps {
   type: "int" | "float" | "text" | "checkbox";
 }
 
 export const CSimpleElement: React.FC<CCP4i2CSimpleElementProps> = (props) => {
-  const { item } = props;
+  const { itemName } = props;
+  const api = useApi();
+  const { data: container } = api.container<any>(
+    `jobs/${props.job.id}/container`
+  );
+  const useItem = useTaskItem(container);
+  const item = useItem(itemName);
 
   const usingSelect = useMemo(() => {
     return item?._qualifiers?.onlyEnumerators;
