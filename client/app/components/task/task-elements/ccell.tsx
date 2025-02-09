de079e07a1a114ac566f5d99aa6d30f90b997868
Martin Noble <martinemnoble1@mac.com>
2025-02-09 13:44:14 +0000

    CCell collapsible

--- client/app/components/task/task-elements/ccell.tsx
+++ client/app/components/task/task-elements/ccell.tsx
@@ -1,23 +1,37 @@
 import { useApi } from "../../../api";
 import { CCP4i2TaskElement, CCP4i2TaskElementProps } from "./task-element";
 import { useJob } from "../task-utils";
-import { Card, CardContent, CardHeader, Grid2 } from "@mui/material";
+import { Button, Card, CardContent, CardHeader, Grid2 } from "@mui/material";
 import { ErrorInfo } from "./error-info";
+import { useState } from "react";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 
 export const CCellElement: React.FC<CCP4i2TaskElementProps> = (props) => {
   const { job, itemName } = props;
   const { getTaskItem, getValidationColor } = useJob(job);
   const item = getTaskItem(itemName);
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <Card sx={{ border: "3px solid", borderColor: getValidationColor(item) }}>
       <CardHeader
-        title={item._qualifiers.guiLabel}
+        title={item?._qualifiers?.guiLabel}
         sx={{ backgroundColor: getValidationColor(item) }}
-        action={<ErrorInfo {...props} />}
+        action={
+          <>
+            <ErrorInfo {...props} />
+            <Button
+              onClick={() => {
+                setExpanded(!expanded);
+              }}
+            >
+              {expanded ? <ExpandLess /> : <ExpandMore />}
+            </Button>
+          </>
+        }
       />
       <CardContent sx={{ my: 0, py: 0 }}>
-        {item && (
+        {item && expanded && (
           <Grid2 container rowSpacing={0} sx={{ my: 0, py: 0 }}>
             {Object.keys(item._value).map((objectKey: string) => (
               <Grid2 key={objectKey} size={{ xs: 4 }}>
