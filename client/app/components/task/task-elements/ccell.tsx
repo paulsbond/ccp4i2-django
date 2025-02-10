c1b93cc1f34bc24e4a21ea04f1c2f24358ed2c6e
Martin Noble <martinemnoble1@mac.com>
2025-02-10 22:17:52 +0000

    Deleting unused imports

--- client/app/components/task/task-elements/ccell.tsx
+++ client/app/components/task/task-elements/ccell.tsx
@@ -1,10 +1,4 @@
-import { useApi } from "../../../api";
-import { CCP4i2TaskElement, CCP4i2TaskElementProps } from "./task-element";
-import { useJob } from "../../../utils";
-import { Button, Card, CardContent, CardHeader, Grid2 } from "@mui/material";
-import { ErrorInfo } from "./error-info";
-import { useMemo, useState } from "react";
-import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { CCP4i2TaskElementProps } from "./task-element";
 import { CContainerElement } from "./ccontainer";
 
 export const CCellElement: React.FC<CCP4i2TaskElementProps> = (props) => (
