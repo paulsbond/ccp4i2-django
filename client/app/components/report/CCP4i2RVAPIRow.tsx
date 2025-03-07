01f82d7b620c4bb2c4869444622e7a5664d09bb5
Martin Noble <martinemnoble1@mac.com>
2025-03-07 14:25:15 +0000

    chart js graphs in for RVApi plots

--- client/app/components/report/CCP4i2RVAPIRow.tsx
+++ client/app/components/report/CCP4i2RVAPIRow.tsx
@@ -3,6 +3,7 @@ import $ from "jquery";
 import { Grid, Grid2 } from "@mui/material";
 import CCP4i2ReportFlotWidget from "./CCP4i2ReportFlotWidget";
 import { CCP4i2ReportElementProps } from "./CCP4i2ReportElement";
+import { CCP4i2ApplicationOutputView } from "./CCP4i2ApplicationOutputView";
 
 export const CCP4i2RVAPIRow: React.FC<CCP4i2ReportElementProps> = (props) => {
   const [content, setContent] = useState<
@@ -16,13 +17,16 @@ export const CCP4i2RVAPIRow: React.FC<CCP4i2ReportElementProps> = (props) => {
         if ($(col).find("div[data-renderer]").length > 0) {
           return (
             <Grid2 size={{ xs: 6 }} key={iCol}>
-              <CCP4i2ReportFlotWidget
-                //Might the following line be because col is found bysearching from dt element
-                //@ts-ignore
-                item={$(col)}
-                iItem={iCol}
-                uniqueId={$(col).find("div[data-renderer]").data("data")}
-              />
+              <CCP4i2ApplicationOutputView output={col} />
+              {false && (
+                <CCP4i2ReportFlotWidget
+                  //Might the following line be because col is found bysearching from dt element
+                  //@ts-ignore
+                  item={$(col)}
+                  iItem={iCol}
+                  uniqueId={$(col).find("div[data-renderer]").data("data")}
+                />
+              )}
             </Grid2>
           );
         } else {
