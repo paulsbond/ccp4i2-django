cfa7099326c031cc66ec14ac4795555c7a69f19c
Martin Noble <martinemnoble1@mac.com>
2025-03-16 21:45:23 +0000

    Callable logic to preview file

---
+++ server/ccp4x/lib/job_utils/preview_file.py
@@ -0,0 +1,43 @@
+import pathlib
+import subprocess
+
+
+def preview_file(viewer: str = None, file_path: pathlib.Path = None):
+    """
+    Preview a file using the appropriate viewer.
+
+    Args:
+        viewer (str, optional): The name of the viewer to use. Defaults to None.
+        file_path (pathlib.Path, optional): The path to the file to be previewed. Defaults to None.
+
+    Returns:
+        None
+
+    Raises:
+        ValueError: If the viewer is not supported.
+    """
+    if viewer == "coot":
+        # Here should dial into mechanism to use the user-specified path to coot as appropriate.
+        # Also suspect will need diverse environment variables to be set for this to work.
+        subprocess.Popen(
+            ["coot", str(file_path)],
+            start_new_session=True,
+        )
+        return {"status": "Success"}
+    if viewer == "ccp4mg":
+        # Here should dial into mechanism to use the user-specified path to coot as appropriate.
+        # Also suspect will need diverse environment variables to be set for this to work.
+        subprocess.Popen(
+            ["ccp4mg", str(file_path)],
+            start_new_session=True,
+        )
+        return {"status": "Success"}
+    if viewer == "hklview":
+        # Here should dial into mechanism to use the user-specified path to coot as appropriate.
+        # Also suspect will need diverse environment variables to be set for this to work.
+        subprocess.Popen(
+            ["hklview", str(file_path)],
+            start_new_session=True,
+        )
+        return {"status": "Success"}
+    raise ValueError(f"Viewer {viewer} is not supported.")
