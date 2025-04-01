27a03f3f1add8380c0dab92c28d77fee692a8bef
Martin Noble <martinemnoble1@mac.com>
2025-04-01 10:37:34 +0100

    Making page display depend on whether projects received: causing projects to be followed

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,25 @@
-import { Container, Stack } from "@mui/material";
+"use client";
+import { Container, Skeleton, Stack } from "@mui/material";
 import MenuBar from "./components/menu-bar";
 import ProjectsToolbar from "./components/projects-toolbar";
 import ProjectsTable from "./components/projects-table";
+import { useApi } from "./api";
+import { Project } from "./models";
 
 export default function ProjectsPage() {
+  const api = useApi();
+  const { data: projects } = api.follow<Project[]>("projects", 2000);
+
   return (
     <Container sx={{ my: 3 }}>
-      <Stack spacing={2}>
-        <ProjectsToolbar />
-        <ProjectsTable />
-      </Stack>
+      {projects ? (
+        <Stack spacing={2}>
+          <ProjectsToolbar />
+          <ProjectsTable />
+        </Stack>
+      ) : (
+        <Skeleton variant="rectangular" width="100%" height={500} />
+      )}
     </Container>
   );
 }
