1dd59f436502069c53b4b4af42879dc8a2773bc2
Martin Noble <martinemnoble1@mac.com>
2025-04-07 22:19:16 +0100

    Handling changes of some file names to ccp4i2

--- client/ccp4i2-master.cjs
+++ client/ccp4i2-master.cjs
@@ -113,7 +113,7 @@ async function startCCP4i2() {
   server.use((req, res, next) => {
     res.setHeader(
       "Content-Security-Policy",
-      `default-src 'self'; connect-src http://localhost:${NEXT_PORT}; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'`
+      `default-src 'self'; connect-src http://localhost:${NEXT_PORT} ; style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline'; script-src 'self' https://cdn.jsdelivr.net 'unsafe-inline' 'unsafe-eval'`
     );
     next();
   });
