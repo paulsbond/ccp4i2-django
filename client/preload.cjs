cd05f680f95a71e58d322aaa64f0ccba3919da81
Martin Noble <martinemnoble1@mac.com>
2025-04-08 14:58:57 +0100

    Preparation for allowing secure interprocess communication

---
+++ client/preload.cjs
@@ -0,0 +1,5 @@
+const { contextBridge, ipcRenderer } = require("electron");
+contextBridge.exposeInMainWorld("electron", {
+  sendMessage: (channel, data) => ipcRenderer.send(channel, data),
+  onMessage: (channel, callback) => ipcRenderer.on(channel, callback),
+});
