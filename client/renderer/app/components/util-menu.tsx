724978962a9f738b26150433db2492a5f2a024bf
Martin Noble <martinemnoble1@mac.com>
2025-04-14 15:57:28 +0100

    Using System Administrator Toolsto reveal /config

--- client/renderer/app/components/util-menu.tsx
+++ client/renderer/app/components/util-menu.tsx
@@ -1,15 +1,21 @@
 "use client";
 import { useState } from "react";
 import { Button, Menu, MenuItem } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 export default function UtilMenu() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const router = useRouter();
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+  const handleSystemAdministratorToolsClick = () => {
+    router.push("/config");
+    handleClose();
   };
 
   return (
@@ -22,7 +28,9 @@ export default function UtilMenu() {
         <MenuItem onClick={handleClose}>Running jobs and processes</MenuItem>
         <MenuItem onClick={handleClose}>Manage imported files</MenuItem>
         <MenuItem onClick={handleClose}>Send error report</MenuItem>
-        <MenuItem onClick={handleClose}>System administrator tools</MenuItem>
+        <MenuItem onClick={handleSystemAdministratorToolsClick}>
+          System administrator tools
+        </MenuItem>
         <MenuItem onClick={handleClose}>Developer tools</MenuItem>
       </Menu>
     </>
