f91655526b0eee5d81cd4adb91e0aac6e656f2fe
Martin Noble <martinemnoble1@mac.com>
2025-04-14 15:11:04 +0100

    Refactoring config-content in light of updated method of getting file existence

--- client/renderer/app/components/config-content.tsx
+++ client/renderer/app/components/config-content.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { PropsWithChildren, useContext } from "react";
+import React, { PropsWithChildren, useCallback, useContext } from "react";
 import {
   Button,
   Container,
@@ -21,34 +21,105 @@ export const ConfigContent: React.FC = () => {
   const [config, setConfig] = useState<any | null>(null);
   const router = useRouter();
   const { devMode, setDevMode } = useContext(CCP4i2Context);
+  const [existingFiles, setExistingFiles] = useState<any | null>(null);
 
   useEffect(() => {
     // Send a message to the main process to get the config
     if (window.electronAPI) {
       window.electronAPI.sendMessage("get-config");
       // Listen for messages from the main process
-      window.electronAPI.onMessage(
-        "message-from-main",
-        (event: any, data: any) => {
-          if (data.message === "get-config") {
-            setConfig(data.config);
-            setDevMode(data.config.devMode);
-          } else if (data.message === "start-uvicorn") {
-            router.push("/");
-          }
-        }
-      );
-    } else console.log("window.electron is not available");
+
+      window.electronAPI.onMessage("message-from-main", messageHandler);
+
+      return () => {
+        window.electronAPI.removeMessageListener(
+          "message-from-main",
+          messageHandler
+        );
+      };
+    } else {
+      console.log("window.electron is not available");
+    }
   }, []);
 
+  const messageHandler = useCallback(
+    (event: any, data: any) => {
+      if (data.message === "get-config") {
+        setConfig(data.config);
+        setDevMode(data.config.devMode);
+      } else if (data.message === "start-uvicorn") {
+        router.push("/");
+      } else if (data.message === "check-file-exists") {
+        if (config) {
+          if (data.path === config.CCP4I2_PROJECTS_DIR) {
+            setExistingFiles((prevState: any) => ({
+              ...prevState,
+              CCP4I2_PROJECTS_DIR: data.exists,
+            }));
+          }
+          if (data.path === config.CCP4Dir) {
+            setExistingFiles((prevState: any) => ({
+              ...prevState,
+              CCP4Dir: data.exists,
+            }));
+          }
+          if (data.path === config.ccp4_python) {
+            setExistingFiles((prevState: any) => ({
+              ...prevState,
+              ccp4_python: data.exists,
+            }));
+          }
+        }
+      }
+    },
+    [config]
+  );
+
+  useEffect(() => {
+    if (config) {
+      window.electronAPI.removeMessageListener(
+        "message-from-main",
+        messageHandler
+      );
+      window.electronAPI.onMessage("message-from-main", messageHandler);
+      if (typeof window !== "undefined" && window.electronAPI) {
+        window.electronAPI.sendMessage("check-file-exists", {
+          path: config.CCP4I2_PROJECTS_DIR,
+        });
+        window.electronAPI.sendMessage("check-file-exists", {
+          path: config.CCP4Dir,
+        });
+        window.electronAPI.sendMessage("check-file-exists", {
+          path: config.ccp4_python,
+        });
+      }
+    }
+    return () => {
+      window.electronAPI.removeMessageListener(
+        "message-from-main",
+        messageHandler
+      );
+    };
+  }, [config]);
+
   const onLaunchBrowser = async () => {
-    if (!window.electronAPI) {
+    if (!window?.electronAPI) {
       console.error("Electron API is not available");
       return;
     }
     console.log("Gonna send locate-ccp4");
     console.log(window.electronAPI);
     window.electronAPI.sendMessage("locate-ccp4");
+  };
+
+  const onSelectProjectsDir = async () => {
+    if (!window.electronAPI) {
+      console.error("Electron API is not available");
+      return;
+    }
+    console.log("Gonna send locate-ccp4");
+    console.log(window.electronAPI);
+    window.electronAPI.sendMessage("locate-ccp4i2-project-directory");
   };
 
   const onStartUvicorn = async () => {
@@ -81,27 +152,53 @@ export const ConfigContent: React.FC = () => {
                 <TableCell variant="head"> </TableCell>
                 <TableCell variant="head"> </TableCell>
                 <TableCell variant="head">Exists ?</TableCell>
+                <TableCell variant="head"> </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               <TableRow>
                 <TableCell variant="head">CCP4Dir</TableCell>
-                <TableCell variant="body">{config.CCP4Dir.path}</TableCell>
+                <TableCell variant="body">{config.CCP4Dir}</TableCell>
                 <TableCell variant="body">
-                  {config.CCP4Dir.exists ? <Check /> : <Cancel />}
+                  {existingFiles?.CCP4Dir ? "Y" : "N"}
+                </TableCell>
+                <TableCell variant="body">
+                  <Button
+                    component="label"
+                    variant="contained"
+                    startIcon={<Folder />}
+                    onClick={onLaunchBrowser}
+                    sx={{ minWidth: 320 }}
+                  >
+                    Select...
+                  </Button>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell variant="head">CCP4Python</TableCell>
-                <TableCell variant="body">{config.CCP4Python.path}</TableCell>
+                <TableCell variant="body">{config.ccp4_python}</TableCell>
                 <TableCell variant="body">
-                  {config.CCP4Python.exists ? <Check /> : <Cancel />}
+                  {existingFiles?.ccp4_python ? "Y" : "N"}
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell variant="head">CCP4I2_PROJECTS_DIR</TableCell>
-                <TableCell variant="body" colSpan={3}>
+                <TableCell variant="body">
                   {config.CCP4I2_PROJECTS_DIR}
+                </TableCell>
+                <TableCell variant="body">
+                  {existingFiles?.CCP4I2_PROJECTS_DIR ? "Y" : "N"}
+                </TableCell>
+                <TableCell variant="body">
+                  <Button
+                    component="label"
+                    variant="contained"
+                    startIcon={<Folder />}
+                    onClick={onSelectProjectsDir}
+                    sx={{ minWidth: 320 }}
+                  >
+                    Select...
+                  </Button>
                 </TableCell>
               </TableRow>
               <TableRow>
@@ -116,25 +213,16 @@ export const ConfigContent: React.FC = () => {
                   {config.UVICORN_PORT}
                 </TableCell>
               </TableRow>
+              <TableRow>
+                <TableCell variant="head">Dev. mode</TableCell>
+                <TableCell variant="body" colSpan={3}>
+                  {devMode ? <Check /> : <Cancel />}
+                </TableCell>
+              </TableRow>
             </TableBody>
-            <TableRow>
-              <TableCell variant="head">Dev. mode</TableCell>
-              <TableCell variant="body" colSpan={3}>
-                {devMode ? <Check /> : <Cancel />}
-              </TableCell>
-            </TableRow>
           </Table>
         )}
         <Stack spacing={2} direction="row">
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<Folder />}
-            onClick={onLaunchBrowser}
-            sx={{ minWidth: 320 }}
-          >
-            Browse to find CCP4
-          </Button>
           <Button
             key="start-uvicorn"
             component="label"
@@ -142,7 +230,7 @@ export const ConfigContent: React.FC = () => {
             startIcon={<Folder />}
             onClick={onStartUvicorn}
             sx={{ minWidth: 320 }}
-            disabled={!config?.CCP4Python.exists}
+            disabled={!existingFiles?.ccp4_python}
           >
             Launch CCP4i2
           </Button>
