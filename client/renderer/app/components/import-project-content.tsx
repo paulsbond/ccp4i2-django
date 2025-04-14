846fc0b80d5f135ad55ddaccc2aec925b88e58f7
Martin Noble <martinemnoble1@mac.com>
2025-04-14 10:57:23 +0100

    Some formating of ImportProjectContent

--- client/renderer/app/components/import-project-content.tsx
+++ client/renderer/app/components/import-project-content.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { PropsWithChildren, useContext } from "react";
+import React, { PropsWithChildren, useContext, useState } from "react";
 import {
   Button,
   Container,
@@ -9,29 +9,32 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 import { useApi } from "../api";
 import { Cancel, Check, Folder, Upload } from "@mui/icons-material";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { CCP4i2Context } from "../app-context";
 import { VisuallyHiddenInput } from "./task/task-elements/input-file-upload";
 
 export const ImportProjectContent: React.FC = () => {
   const api = useApi();
+  const [files, setFiles] = useState<File[]>([]);
+  const [uploading, setUploading] = useState(false);
+
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
+    setFiles(files ? Array.from(files) : []);
     if (files) {
       const formData = new FormData();
       for (let i = 0; i < files.length; i++) {
         formData.append("files", files[i]);
       }
       // Handle the form data as needed (e.g., send it to an API)
-      console.log("Files selected:", files);
+      setUploading(true);
       api
         .post<any>("/projects/import_project/", formData)
         .then((response) => {
           console.log("Files uploaded successfully:", response.data);
+          setUploading(false);
         })
         .catch((error) => {
           console.error("Error uploading files:", error);
@@ -45,16 +48,45 @@ export const ImportProjectContent: React.FC = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "100vh", // Full viewport height
+        height: "calc(100vh - 5rem)", // Full viewport height
         margin: 0, // Remove default margin
       }}
     >
       <Stack spacing={2}>
+        <Typography variant="h4" gutterBottom>
+          Import Project
+        </Typography>
         <Stack spacing={2} direction="row">
           <Button component="label" variant="contained" startIcon={<Upload />}>
             <VisuallyHiddenInput type="file" multiple onChange={onChange} />
           </Button>
         </Stack>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>File</TableCell>
+              <TableCell>Processing</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* Map through the files and display their names and statuses */}
+            {/* Example static data for demonstration */}
+            {files.map((aFile, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  <Folder /> {aFile.name}
+                </TableCell>
+                <TableCell>
+                  {!uploading ? (
+                    <Check color="success" />
+                  ) : (
+                    <Cancel color="error" />
+                  )}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Stack>
     </Container>
   );
