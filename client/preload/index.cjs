10660c493a48dd20eebaf9b55b647b50be09d5c8
Martin Noble <martinemnoble1@mac.com>
2025-04-14 15:10:31 +0100

    Also in index.cjs

--- client/preload/index.cjs
+++ client/preload/index.cjs
@@ -3,5 +3,8 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("electronAPI", {
   sendMessage: (channel, data) => ipcRenderer.send(channel, data),
   onMessage: (channel, callback) => ipcRenderer.on(channel, callback),
+  removeMessageListener: (channel, callback) => {
+    ipcRenderer.removeListener(channel, callback);
+  },
 });
 console.log("[Preload] Script loaded");
