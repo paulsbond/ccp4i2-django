ff1a0149ae23dc54051f81b6315c2183ab434739
Martin Noble <martinemnoble1@mac.com>
2025-04-22 17:14:02 +0100

    Separating out the Coot loading stuff into a COotProvider

--- client/renderer/app/project/[id]/layout.tsx
+++ client/renderer/app/project/[id]/layout.tsx
@@ -1,37 +1,14 @@
 "use client";
-import {
-  PropsWithChildren,
-  use,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { PropsWithChildren, use, useContext, useEffect, useState } from "react";
 import { Paper, Stack, Tab, Tabs } from "@mui/material";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import { CCP4i2Context } from "../../app-context";
 import { useApi } from "../../api";
-import Script from "next/script";
 import { CCP4i2DirectoryViewer } from "../../components/directory_viewer";
 import { useProject } from "../../utils";
 import { ClassicJobList } from "../../components/classic-jobs-list";
 import { DraggableContext } from "../../components/draggable-context";
-
-const createArgs = {
-  print(t: string) {
-    console.log(["output", t]);
-  },
-  printErr(t: string) {
-    console.error(["output", t]);
-  },
-  locateFile(path: string, prefix: string) {
-    // if it's moorhen.wasm, use a custom dir
-    if (path.endsWith("moorhen.wasm")) return "/moorhen.wasm";
-    if (path.endsWith("mtz.wasm")) return prefix + path;
-    // otherwise, use the default, the prefix (JS file's dir) + the path
-    return prefix + path;
-  },
-};
+import { CootProvider } from "../../components/coot-provider";
 
 export interface ProjectLayoutProps extends PropsWithChildren {
   params: Promise<{ id: string; jobid: string }>;
@@ -44,15 +21,6 @@ export default function ProjectLayout(props: ProjectLayoutProps) {
   const [tabValue, setTabValue] = useState(0);
   const { id } = use(props.params);
   const { project } = useProject(parseInt(id));
-  const scriptElement = useRef<HTMLElement | null | undefined>(null);
-
-  useEffect(() => {
-    return () => {
-      if (scriptElement.current) {
-        scriptElement.current.parentElement?.removeChild(scriptElement.current);
-      }
-    };
-  }, []);
 
   useEffect(() => {
     const asyncFunc = async () => {
@@ -73,75 +41,56 @@ export default function ProjectLayout(props: ProjectLayoutProps) {
         spacing={2}
         sx={{ height: "calc(100vh - 4rem)", paddingTop: "1rem", width: "100%" }}
       >
-        {true && (
-          <Script
-            src="/moorhen.js"
-            strategy="lazyOnload"
-            id="moorhen-script-element"
-            onLoad={async (arg) => {
-              const cootModule =
-                //@ts-ignore
-                createCootModule(createArgs).then((module: any) => {
-                  //@ts-ignore
-                  setCootModule(module);
-                  scriptElement.current = Array.from(
-                    document.getElementsByTagName("script")
-                  ).find((htmlElement: HTMLElement) => {
-                    return htmlElement.getAttribute("src") === "/moorhen.js";
-                  });
-                  console.log({ cm: module, scriptElement });
-                });
-            }}
-          />
-        )}
-        <PanelGroup direction="horizontal">
-          <Panel defaultSize={30} minSize={20}>
-            <Paper sx={{ overflowY: "auto", height: "calc(100vh - 8rem)" }}>
-              <Tabs
-                value={tabValue}
-                onChange={handleTabChange}
-                variant="fullWidth"
-              >
-                <Tab value={0} label="Job list" />
-                {/*<Tab value={1} label="Job grid" />*/}
-                <Tab value={2} label="Project directory" />
-              </Tabs>
-              {tabValue == 0 && project && (
-                <ClassicJobList projectId={project.id} />
-              )}
-              {/*tabValue == 1 && <JobsGrid projectId={id} size={{ xs: 12 }} />*/}
-              {tabValue == 2 && project && (
-                <CCP4i2DirectoryViewer projectId={project.id} />
-              )}
-            </Paper>
-          </Panel>
-          <PanelResizeHandle
-            style={{
-              width: 10,
-              backgroundColor: "transparent",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              cursor: "col-resize",
-            }}
-          >
-            <div
+        <CootProvider>
+          <PanelGroup direction="horizontal">
+            <Panel defaultSize={30} minSize={20}>
+              <Paper sx={{ overflowY: "auto", height: "calc(100vh - 8rem)" }}>
+                <Tabs
+                  value={tabValue}
+                  onChange={handleTabChange}
+                  variant="fullWidth"
+                >
+                  <Tab value={0} label="Job list" />
+                  {/*<Tab value={1} label="Job grid" />*/}
+                  <Tab value={2} label="Project directory" />
+                </Tabs>
+                {tabValue == 0 && project && (
+                  <ClassicJobList projectId={project.id} />
+                )}
+                {/*tabValue == 1 && <JobsGrid projectId={id} size={{ xs: 12 }} />*/}
+                {tabValue == 2 && project && (
+                  <CCP4i2DirectoryViewer projectId={project.id} />
+                )}
+              </Paper>
+            </Panel>
+            <PanelResizeHandle
               style={{
-                width: 4,
-                height: "50%",
-                backgroundColor: "gray",
-                borderRadius: 2,
+                width: 10,
+                backgroundColor: "transparent",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                cursor: "col-resize",
               }}
-            />
-          </PanelResizeHandle>
-          <Panel
-            defaultSize={70}
-            minSize={20}
-            onResize={(size) => setJobPanelSize && setJobPanelSize(size)}
-          >
-            {props.children}
-          </Panel>
-        </PanelGroup>
+            >
+              <div
+                style={{
+                  width: 4,
+                  height: "50%",
+                  backgroundColor: "gray",
+                  borderRadius: 2,
+                }}
+              />
+            </PanelResizeHandle>
+            <Panel
+              defaultSize={70}
+              minSize={20}
+              onResize={(size) => setJobPanelSize && setJobPanelSize(size)}
+            >
+              {props.children}
+            </Panel>
+          </PanelGroup>
+        </CootProvider>
       </Stack>
     </DraggableContext>
   );
