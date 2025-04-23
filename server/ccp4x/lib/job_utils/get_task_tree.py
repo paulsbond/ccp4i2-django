e552fdb2dd3440762c716df958422870c883dff3
Martin Noble <martinemnoble1@mac.com>
2025-04-23 16:40:33 +0100

    Providing icon lookup

--- server/ccp4x/lib/job_utils/get_task_tree.py
+++ server/ccp4x/lib/job_utils/get_task_tree.py
@@ -3,5 +3,9 @@ from ccp4i2.core.CCP4TaskManager import CTaskManager
 
 def get_task_tree():
     taskManager = CTaskManager()
-    result = {"tree": taskManager.taskTree(), "lookup": taskManager.taskLookup}
+    result = {
+        "tree": taskManager.taskTree(),
+        "lookup": taskManager.taskLookup,
+        "iconLookup": taskManager.taskIconLookup,
+    }
     return result
