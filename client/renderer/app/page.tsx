270d823815435806e502db95edd49e6666997fb1
Martin Noble <martinemnoble1@mac.com>
2025-05-13 09:34:54 +0100

    Pre fetching tasks for snappier later rendering

--- client/renderer/app/page.tsx
+++ client/renderer/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Container, Skeleton, Stack } from "@mui/material";
+import { Badge, Container, Skeleton, Stack } from "@mui/material";
 import ProjectsToolbar from "./components/projects-toolbar";
 import ProjectsTable from "./components/projects-table";
 import { useApi } from "./api";
@@ -8,10 +8,17 @@ import { Project } from "./models";
 export default function ProjectsPage() {
   const api = useApi();
   const { data: projects } = api.get<Project[]>("projects", 1000);
+  const { data: task_tree } = api.get<any>(`task_tree/`);
 
   return (
     <Container sx={{ my: 3 }}>
       <Stack spacing={2}>
+        {task_tree?.task_tree?.tree && (
+          <Badge badgeContent={task_tree.task_tree.tree.length} color="primary">
+            {" "}
+            Tasks available
+          </Badge>
+        )}
         <ProjectsToolbar />
         {projects && projects.length > 0 ? (
           <ProjectsTable />
