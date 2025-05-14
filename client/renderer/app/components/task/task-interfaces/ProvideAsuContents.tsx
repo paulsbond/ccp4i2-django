9d49838f9461f53e757b7e8e08ab56cda8c666e9
Martin Noble <martinemnoble1@mac.com>
2025-05-14 19:59:33 +0100

    Adop contents of  ASU file if one gets set

--- client/renderer/app/components/task/task-interfaces/ProvideAsuContents.tsx
+++ client/renderer/app/components/task/task-interfaces/ProvideAsuContents.tsx
@@ -2,17 +2,19 @@ import { Grid2, LinearProgress, Paper, Stack, Typography } from "@mui/material";
 import { CCP4i2TaskInterfaceProps } from "../task-container";
 import { CCP4i2TaskElement } from "../task-elements/task-element";
 import { CCP4i2Tab, CCP4i2Tabs } from "../task-elements/tabs";
-import { useApi } from "../../../api";
-import { useJob, usePrevious } from "../../../utils";
+import { fullUrl, useApi } from "../../../api";
+import { useJob, usePrevious, valueOfItem } from "../../../utils";
 import { CContainerElement } from "../task-elements/ccontainer";
 import { useCallback, useEffect, useMemo } from "react";
 
 const TaskInterface: React.FC<CCP4i2TaskInterfaceProps> = (props) => {
   const api = useApi();
   const { job } = props;
-  const { setParameter, useAsyncEffect, getTaskItem, getFileDigest } = useJob(
+  const { setParameter, useAsyncEffect, getTaskItem, mutateContainer } = useJob(
     job.id
   );
+  const { update: setAsuContent } = getTaskItem("ASU_CONTENT");
+
   //const { value: ID_RMSValue } = getTaskItem("ID_RMS");
 
   //These here to show how the Next useSWR aproach can furnish up to date digests of nput files
@@ -21,6 +23,19 @@ const TaskInterface: React.FC<CCP4i2TaskInterfaceProps> = (props) => {
   //);
 
   //This magic means that the following variables will be kept up to date with the values of the associated parameters
+
+  const handleNewASUCONTENTIN = useCallback(
+    async (updatedItem: any) => {
+      if (!setAsuContent) return;
+      const { dbFileId } = valueOfItem(updatedItem);
+      const digest = await fetch(
+        fullUrl(`files/${dbFileId}/digest_by_uuid/`)
+      ).then((response) => response.json());
+      await setAsuContent(digest.seqList);
+      mutateContainer();
+    },
+    [setAsuContent]
+  );
 
   return (
     <CCP4i2Tabs {...props}>
@@ -39,6 +54,7 @@ const TaskInterface: React.FC<CCP4i2TaskInterfaceProps> = (props) => {
             {...props}
             itemName="ASUCONTENTIN"
             qualifiers={{ guiLabel: "ASU contents" }}
+            onParameterChangeSuccess={handleNewASUCONTENTIN}
           />
         </CContainerElement>
         <CContainerElement
