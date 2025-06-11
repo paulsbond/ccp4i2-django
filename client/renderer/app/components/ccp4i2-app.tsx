71ee84215f0c3cfa61bfde080f2c30b5e1aabba2
Martin Noble <martinemnoble1@mac.com>
2025-06-11 14:31:26 +0100

    Popcorn provider

--- client/renderer/app/components/ccp4i2-app.tsx
+++ client/renderer/app/components/ccp4i2-app.tsx
@@ -5,6 +5,7 @@ import { CssBaseline } from "@mui/material";
 import MenuBar from "./menu-bar";
 import { File, Job } from "../models";
 import { RunningProcessesProvider } from "./running-processes";
+import { PopcornProvider } from "./popcorn-provider";
 
 export const CCP4i2App = (props: PropsWithChildren) => {
   const [projectId, setProjectId] = useState<number | null>(null);
@@ -15,27 +16,29 @@ export const CCP4i2App = (props: PropsWithChildren) => {
   const [activeDragItem, setActiveDragItem] = useState<Job | File | null>(null);
 
   return (
-    <CCP4i2Context.Provider
-      value={{
-        projectId,
-        setProjectId,
-        jobId,
-        setJobId,
-        cootModule,
-        setCootModule,
-        jobPanelSize,
-        setJobPanelSize,
-        devMode,
-        setDevMode,
-        activeDragItem,
-        setActiveDragItem,
-      }}
-    >
-      <CssBaseline />
-      <RunningProcessesProvider>
-        <MenuBar />
-        {props.children}
-      </RunningProcessesProvider>
-    </CCP4i2Context.Provider>
+    <PopcornProvider>
+      <CCP4i2Context.Provider
+        value={{
+          projectId,
+          setProjectId,
+          jobId,
+          setJobId,
+          cootModule,
+          setCootModule,
+          jobPanelSize,
+          setJobPanelSize,
+          devMode,
+          setDevMode,
+          activeDragItem,
+          setActiveDragItem,
+        }}
+      >
+        <CssBaseline />
+        <RunningProcessesProvider>
+          <MenuBar />
+          {props.children}
+        </RunningProcessesProvider>
+      </CCP4i2Context.Provider>
+    </PopcornProvider>
   );
 };
