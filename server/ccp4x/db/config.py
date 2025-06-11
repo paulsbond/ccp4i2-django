f02d1baa35a0be72e7070cdc09a701a96bde1eda
Martin Noble <martinemnoble1@mac.com>
2025-06-11 14:31:57 +0100

    Set up and exemplify a signal (although not sure of utility yet)

--- server/ccp4x/db/config.py
+++ server/ccp4x/db/config.py
@@ -5,3 +5,7 @@ class DbConfig(AppConfig):
     name = "ccp4x.db"
     label = "ccp4x"
     default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self):
+        print("status: ready")
+        import ccp4x.db.signals  # noqa
