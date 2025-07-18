addeaaaf75bc89bb16a08aeb1d329c8d7704e509
Martin Noble <martinemnoble1@mac.com>
2025-07-18 09:49:01 +0100

    Sidestepping Moorhen dynamic loading (works because now harmonised react versions)

--- client/renderer/app/moorhen-page/file-by-id/[id]/page.tsx
+++ client/renderer/app/moorhen-page/file-by-id/[id]/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { useParams } from "next/navigation";
 import useSWR from "swr";
-import MoorhenLoader from "../../../../components/moorhen/client-side-moorhen-loader";
+import MoorhenWrapper from "../../../../components/moorhen/moorhen-wrapper";
+import { ClientStoreProvider } from "../../../../providers/client-store-provider";
 
 const FileByIdPage = () => {
   const { id } = useParams();
   const { data: file } = useSWR(`/api/proxy/files/${id}`, (url) =>
     fetch(url).then((res) => res.json())
   );
-  return <MoorhenLoader fileIds={id ? [parseInt(id as string)] : []} />;
+  return (
+    <ClientStoreProvider>
+      <MoorhenWrapper fileIds={id ? [parseInt(id as string)] : []} />
+    </ClientStoreProvider>
+  );
 };
 export default FileByIdPage;
