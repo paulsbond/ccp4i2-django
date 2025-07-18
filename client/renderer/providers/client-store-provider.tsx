1a05aed179ff010a9c6e112ab29e28c287365897
Martin Noble <martinemnoble1@mac.com>
2025-07-18 09:46:04 +0100

    Using import instead of require for fetching moorhen reducers; including all exported reducers

--- client/renderer/providers/client-store-provider.tsx
+++ client/renderer/providers/client-store-provider.tsx
@@ -3,28 +3,28 @@
 import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { atomInfoCardsReducer, menusReducer, sliceNDiceReducer } from "moorhen";
+import {
+  moleculesReducer,
+  mapsReducer,
+  mouseSettingsReducer,
+  backupSettingsReducer,
+  shortcutSettingsReducer,
+  labelSettingsReducer,
+  sceneSettingsReducer,
+  generalStatesReducer,
+  hoveringStatesReducer,
+  modalsReducer,
+  mapContourSettingsReducer,
+  moleculeMapUpdateReducer,
+  sharedSessionReducer,
+  refinementSettingsReducer,
+  lhasaReducer,
+  overlaysReducer,
+  glRefSliceReducer,
+} from "moorhen";
 
 export function ClientStoreProvider({ children }: { children: ReactNode }) {
-  const {
-    moleculesReducer,
-    mapsReducer,
-    mouseSettingsReducer,
-    backupSettingsReducer,
-    shortcutSettingsReducer,
-    labelSettingsReducer,
-    sceneSettingsReducer,
-    generalStatesReducer,
-    hoveringStatesReducer,
-    modalsReducer,
-    mapContourSettingsReducer,
-    moleculeMapUpdateReducer,
-    sharedSessionReducer,
-    refinementSettingsReducer,
-    lhasaReducer,
-    overlaysReducer,
-    glRefSliceReducer,
-  } = require("moorhen");
-
   const store = configureStore({
     reducer: {
       molecules: moleculesReducer,
@@ -42,14 +42,18 @@ export function ClientStoreProvider({ children }: { children: ReactNode }) {
       sharedSession: sharedSessionReducer,
       refinementSettings: refinementSettingsReducer,
       lhasa: lhasaReducer,
-      overlays: overlaysReducer,
+      sliceNDice: sliceNDiceReducer,
+      //jsonValidation: jsonValidationReducer,
+      //mrParse: mrPar,
       glRef: glRefSliceReducer,
+      overlays: overlaysReducer,
+      menus: menusReducer,
+      atomInfoCards: atomInfoCardsReducer,
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
         serializableCheck: false,
       }),
   });
-
   return <Provider store={store}>{children}</Provider>;
 }
