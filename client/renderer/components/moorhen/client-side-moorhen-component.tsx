0d7b7e1d4246d8c2fa7a247045b35b29de54fe08
Martin Noble <martinemnoble1@mac.com>
2025-07-25 08:11:34 +0100

    Additional guard on rendering moorhenWrapper

--- client/renderer/components/moorhen/client-side-moorhen-component.tsx
+++ client/renderer/components/moorhen/client-side-moorhen-component.tsx
@@ -4,12 +4,14 @@ import { useContext } from "react";
 import { ClientStoreProvider } from "../../providers/client-store-provider";
 import MoorhenWrapper, { MoorhenWrapperProps } from "./moorhen-wrapper";
 import { CCP4i2Context } from "../../app-context";
+import { useStore } from "react-redux";
 
 const ClientSideMoorhenComponent: React.FC<MoorhenWrapperProps> = (props) => {
   const { cootModule } = useContext(CCP4i2Context);
+  const store = useStore();
   return (
     <ClientStoreProvider>
-      {cootModule && <MoorhenWrapper {...props} />}
+      {cootModule && store && <MoorhenWrapper {...props} />}
     </ClientStoreProvider>
   );
 };
