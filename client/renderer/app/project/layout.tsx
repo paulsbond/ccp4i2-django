9a0aba0bbce53141430129f85613f0ff44e257db
Martin Noble <martinemnoble1@mac.com>
2025-07-25 08:32:52 +0100

    Relocating RuningProcessesProvider to avoid calls before backend initiated

--- client/renderer/app/project/layout.tsx
+++ client/renderer/app/project/layout.tsx
@@ -2,13 +2,16 @@
 import { PropsWithChildren } from "react";
 import { CootProvider } from "../../providers/coot-provider";
 import { RdkitProvider } from "../../providers/rdkit-provider";
+import { RunningProcessesProvider } from "../../providers/running-processes";
 
 export default function ProjectLayout(props: PropsWithChildren) {
   return (
     <CootProvider>
       <RdkitProvider>
-        {/* Children components will be rendered here */}
-        {props.children}
+        <RunningProcessesProvider>
+          {/* Children components will be rendered here */}
+          {props.children}
+        </RunningProcessesProvider>
       </RdkitProvider>
     </CootProvider>
   );
