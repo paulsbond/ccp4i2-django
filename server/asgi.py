183962caa43c01e10f4799592cca978a29898471
Martin Noble <martinemnoble1@mac.com>
2025-10-04 12:20:06 +0000

    DLL discovery was an issue on windows

--- server/asgi.py
+++ server/asgi.py
@@ -9,6 +9,15 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+# MINIMAL FIX: Add CCP4 DLL directory for Windows
+if os.name == "nt" and hasattr(os, "add_dll_directory"):
+    ccp4_dir = os.environ.get("CCP4")
+    if ccp4_dir:
+        try:
+            os.add_dll_directory(os.path.join(ccp4_dir, "bin"))
+        except (OSError, AttributeError):
+            pass
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ccp4x.config.settings")
